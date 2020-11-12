3c6d2ca4115134ce68a7f10048be3dd11ac63bf9
unknown <adikmutia@gmail.com>
2020-11-12 18:37:25 +0700

    first commit

---
+++ services/handler_fastpay.go
@@ -0,0 +1,17 @@
+package services
+
+import (
+	"context"
+
+	cm "FrameworkMutia/git/customer/common"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+
+func (PaymentService) FastPayHandler(ctx context.Context, req cm.FastPayRequest) (res cm.FastPayResponse) {
+
+	
+
+	return
+}
